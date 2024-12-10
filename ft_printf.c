e6cf899cb00aa6f66ede2b27689c2070a711ab2f
Loic Maria <lmaria@z2r12p7.42lyon.fr>
2024-12-10 19:45:24 +0100

    add protection stdout

--- ft_printf.c
+++ ft_printf.c
@@ -6,13 +6,13 @@
 /*   By: lmaria <lmaria@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/12/09 12:56:28 by lmaria            #+#    #+#             */
-/*   Updated: 2024/12/10 15:33:04 by lmaria           ###   ########.fr       */
+/*   Updated: 2024/12/10 19:42:35 by lmaria           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_printf.h"
 
-static int	process_format2(const char *format, va_list args, int count,
+static int	process_conversion(const char *format, va_list args, int count,
 		unsigned long ptr)
 {
 	if (*format == 'c')
@@ -40,23 +40,25 @@ static int	process_format2(const char *format, va_list args, int count,
 	return (count);
 }
 
-static int	process_format(const char *format, va_list args)
+static int	process_format_string(const char *format, va_list args, int count)
 {
-	int				count;
 	unsigned long	ptr;
 
-	count = 0;
 	ptr = 0;
+	if (write(1, "", 0) == -1)
+		return (-1);
 	while (*format)
 	{
 		if (*format == '%')
 		{
 			format++;
-			count = process_format2(format, args, count, ptr);
+			count = process_conversion(format, args, count, ptr);
 		}
 		else
 		{
-			count += ft_putchar(*format);
+			if (ft_putchar(*format) == -1)
+				return (-1);
+			count++;
 		}
 		format++;
 	}
@@ -68,8 +70,23 @@ int	ft_printf(const char *format, ...)
 	va_list	args;
 	int		count;
 
+	count = 0;
 	va_start(args, format);
-	count = process_format(format, args);
+	count = process_format_string(format, args, count);
 	va_end(args);
 	return (count);
+}
+
+#include <stdio.h>
+
+int	main(void)
+{
+	int	nb;
+	int	nb2;
+
+	nb = ft_printf("abcde % \n");
+	nb2 = printf("abcde % \n");
+	printf("%d\n", nb);
+	printf("%d\n", nb2);
+	return (0);
 }
